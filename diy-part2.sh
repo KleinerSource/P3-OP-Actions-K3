6ccb3f56cf266673dac012bfc86e7e343514c849
KleinerSource <KleinerSource@me.com>
2020-09-29 19:59:41 +0800

    add ssrp

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # 修改默认 IP
-#sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为 Argon
 rm -rf package/lean/luci-theme-argon  
@@ -26,7 +26,7 @@ git clone https://github.com/vernesong/OpenClash.git package/kleinersource/OpenC
 git clone https://github.com/destan19/OpenAppFilter.git package/kleinersource/OpenAppFilter
 
 # Rosy 主题
-#git clone https://github.com/rosywrt/luci-theme-rosy.git package/kleinersource/luci-theme-rosy
+# git clone https://github.com/rosywrt/luci-theme-rosy.git package/kleinersource/luci-theme-rosy
 
 # SmartDNS
 # git clone https://github.com/RealKiro/Luci-SmartDNS.git package/kleinersource/Luci-SmartDNS
@@ -34,12 +34,23 @@ git clone https://github.com/destan19/OpenAppFilter.git package/kleinersource/Op
 # KPR plus+
 git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kleinersource/luci-app-koolproxyR
 
+# Docker
+# git clone  https://github.com/lisaac/luci-lib-docker.git package/kleinersource/luci-lib-docker
+# git clone  https://github.com/lisaac/luci-app-dockerman.git package/kleinersource/luci-app-dockerman        
+
+# SSRP
+git clone https://github.com/fw876/helloworld.git --depth=1 ./tmp/helloworld
+cp -rf ./tmp/helloworld/luci-app-ssr-plus ./package/lean/
+cp -rf ./tmp/helloworld/tcping ./package/lean/
+cp -rf ./tmp/helloworld/naiveproxy ./package/lean/
+rm -rf ./tmp/
+
 # K3
 rm -rf package/lean/k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl package/kleinersource/k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl_build package/kleinersource/k3screenctrl_build
 git clone https://github.com/lwz322/luci-app-k3screenctrl package/kleinersource/luci-app-k3screenctrl
-git clone https://github.com/tty228/luci-app-serverchan.git    package/kleinersource/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/kleinersource/luci-app-serverchan
 
 # 刷新&安装源
 ./scripts/feeds update -a
